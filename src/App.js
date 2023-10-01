6483a9481e0d35a476b9664947a2e41394614655
bassem <basem12683@gmail.com>
2023-10-01 08:35:35 +0300

    main arrange

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
 
 import "bootstrap/dist/css/bootstrap.min.css";
+import Category from "./components/Category";
+import Colors from "./components/Colors";
+import Price from "./components/Price";
+import Navigation from "./components/Navigation";
+import Recommended from "./components/Recommended";
+import Products from "./components/Products";
 
 function App() {
   return (
+    <div className="d-flex">
+    <div className='col-2'>
+    <Category/>
+    <Colors/>
+    <Price/>
+    </div>
     <div >
-      hello world
+    <Navigation/>
+    <Recommended/>
+    <Products/>
+    </div>
     </div>
   );
 }
