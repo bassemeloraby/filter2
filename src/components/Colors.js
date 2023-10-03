4ff0dec5321f03d27fcf6ae7ad67c00c31b69f07
bassem <basem12683@gmail.com>
2023-10-03 03:34:28 +0300

    colors

--- src/components/Colors.js
+++ src/components/Colors.js
@@ -1,8 +1,27 @@
 import React from 'react'
+import Co from '../db/ColorsDb'
 
-const Colors = () => {
+const Colors = ({handleChange}) => {
   return (
-    <div>Colors</div>
+    <div>Colors
+    
+    {Co.map((c, i) => (
+      <div class="form-check" key={i}>
+        <input
+          class="form-check-input"
+          type="radio"
+          name={c.value}
+          id={c.value}
+          onChange={handleChange}
+          value={c.value}
+        />
+        <label class="form-check-label" for={c.value} style={{ backgroundColor:`${c.value}`  }}>
+          {c.title}
+        </label>
+      </div>
+    ))}
+    </div>
+
   )
 }
 
---
+++ src/db/ColorsDb.js
@@ -0,0 +1,40 @@
+const Co = [
+    {
+      title: "All",
+      type: "radio",
+      onChange: "handleChange",
+      value: "",
+      name: "test",
+    },
+    {
+      title: "-------",
+      type: "radio",
+      onChange: "handleChange",
+      value: "black",
+      name: "50",
+    },
+    {
+      title: "-------",
+      type: "radio",
+      onChange: "handleChange",
+      value: "blue",
+      name: "test",
+    },
+    {
+      title: "-------",
+      type: "radio",
+      onChange: "handleChange",
+      value: "red",
+      name: "test",
+    },
+    {
+      title:"-------",
+      type: "radio",
+      onChange: "handleChange",
+      value: "green",
+      name: "test",
+    },
+  ];
+  
+  export default Co;
+  
